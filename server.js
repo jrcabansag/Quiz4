8c47cacdc9eb1d521fc845627ef67dbfb08f53cd
JR <cabansag@stanford.edu>
2017-11-03 23:29:56 -0700

    Made site pretty

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
 var playersNameDictionary = {};
 var lobbyDictionary = {};
 var playersGameCodeDictionary = {};
@@ -242,6 +244,8 @@ function scrambleWord(word){
     }
     return newWord;
 }
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
